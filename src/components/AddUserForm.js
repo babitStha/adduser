26f7de6755ecc8cc6678547cdc1049fa4642f365
babeetstha <babeetstha@gmail.com>
2022-05-14 13:24:24 +0545

    added useRef instead of some useStage

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -1,29 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styles from "./AddUserForm.module.css"
 import Section from "./Section";
 import Button from "./UI/Button";
 import Modal from "./Modal";
 const AddUserForm = (props) =>{
-    const [name, setName] = useState("");
-    const [age, setAge] = useState("");
+    const name = useRef();
+    const age = useRef();
     const [error, setError] = useState()
 
-    const userNameChangeHandler = (event) =>{
-        setName(event.target.value)
-        console.log(event.target.value)
-    }
-    const ageChangeHandler = (event) =>{
-        setAge(event.target.value)
-        console.log(event.target.value)
-    }
 const onSubmitHandler =(event) =>{
     console.log(event)
     const user = {
         id: Math.random().toString(),
-        name: name,
-        age: age,
+        name: name.current.value,
+        age: age.current.value,
     }
-    if ((name.length === 0) || (age.length === 0)){
+    if ((name.current.value.length === 0) || (age.current.value.length === 0)){
         setError({
             title: "Invalid Data",
             body: "Please provide valid Data",
@@ -38,8 +30,8 @@ const onSubmitHandler =(event) =>{
         return
     }
     props.onAddUser(user)
-    setName('')
-    setAge('')
+    name.current.value = ''
+    age.current.value = ''
 }
 const onCloseHandler = ()=>{
     setError(null)
@@ -48,11 +40,11 @@ const onCloseHandler = ()=>{
         <Section className={`${styles['add-user-form']}`}>
             <div className={`${styles['add-user-input']}`}>
                 <label> Username</label>
-                <input type="text" onChange={userNameChangeHandler} value={name}/>
+                <input type="text" ref={name}/>
             </div>
             <div className={`${styles['add-user-input']}`}>
                 <label> Age</label>
-                <input onChange={ageChangeHandler} type="number" value={age}/>
+                <input type="number" ref={age}/>
             </div>
             <Button type="submit" onClick={onSubmitHandler} >Add User</Button>
             {error && <Modal title={error.title} body={error.body} onClose= {onCloseHandler}/>}
